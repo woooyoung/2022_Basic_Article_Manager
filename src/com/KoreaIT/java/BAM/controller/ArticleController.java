ffd1c33cdc5cc5e10ca963e9b5c1b7167ea5615f
woooyoung <axdsw121@gmail.com>
2022-07-25 17:23:58 +0900

    게시물 작성시 게시물에 작성자에 관한 정보를 저장(기준:memberId)

--- src/com/KoreaIT/java/BAM/controller/ArticleController.java
+++ src/com/KoreaIT/java/BAM/controller/ArticleController.java
@@ -58,7 +58,7 @@ public class ArticleController extends Controller {
 		System.out.printf("내용 : ");
 		String body = sc.nextLine();
 
-		Article article = new Article(id, regDate, title, body);
+		Article article = new Article(id, regDate, loginedMember.id, title, body);
 		articles.add(article);
 
 		System.out.printf("%d번 글이 생성되었습니다\n", id);
@@ -92,11 +92,12 @@ public class ArticleController extends Controller {
 			}
 		}
 
-		System.out.printf("번호    |   제목   |   	  %7s        |   조회\n", "날짜");
+		System.out.printf("번호    |   제목     |     %7s        |    작성자  |   조회\n", "날짜");
 		for (int i = forPrintArticles.size() - 1; i >= 0; i--) {
 			Article article = forPrintArticles.get(i);
 
-			System.out.printf("%7d | %6s   | %5s   | %5d\n", article.id, article.title, article.regDate, article.hit);
+			System.out.printf("%7d | %6s   | %5s  |   %7s  | %5d\n", article.id, article.title, article.regDate,
+					article.memberId, article.hit);
 		}
 
 	}
@@ -124,6 +125,7 @@ public class ArticleController extends Controller {
 		System.out.printf("날짜 : %s\n", foundArticle.regDate);
 		System.out.printf("제목 : %s\n", foundArticle.title);
 		System.out.printf("내용 : %s\n", foundArticle.body);
+		System.out.printf("작성자 : %s\n", foundArticle.memberId);
 		System.out.printf("조회 : %d\n", foundArticle.hit);
 
 	}
@@ -203,9 +205,9 @@ public class ArticleController extends Controller {
 	public void makeTestData() {
 		System.out.println("테스트를 위한 게시물 데이터를 생성합니다.");
 
-		articles.add(new Article(1, Util.getNowDateStr(), "제목1", "내용1", 11));
-		articles.add(new Article(2, Util.getNowDateStr(), "제목2", "내용2", 22));
-		articles.add(new Article(3, Util.getNowDateStr(), "제목3", "내용3", 33));
+		articles.add(new Article(1, Util.getNowDateStr(), 1, "제목1", "내용1", 11));
+		articles.add(new Article(2, Util.getNowDateStr(), 2, "제목2", "내용2", 22));
+		articles.add(new Article(3, Util.getNowDateStr(), 2, "제목3", "내용3", 33));
 	}
 
 }
